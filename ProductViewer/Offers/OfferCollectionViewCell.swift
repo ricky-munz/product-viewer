3b6df28b99bba8e36f76097cb1b3ee06455522ab
Ricky Munz <richard.munz@unos.org>
2017-12-10 11:30:30 -0500

    Add OffersCollectionViewCell.

---
+++ ProductViewer/Offers/OfferCollectionViewCell.swift
@@ -0,0 +1,37 @@
+//
+//  OfferCollectionViewCell.swift
+//  ProductViewer
+//
+//  Created by Richard S. Munz on 12/10/17.
+//  Copyright © 2017 Richard S. Munz. All rights reserved.
+//
+
+import UIKit
+
+class OfferCollectionViewCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    let testLabel: UILabel = {
+        let label = UILabel()
+        label.text = "test label"
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    func setupViews() {
+        backgroundColor = UIColor.blue
+        
+        addSubview(testLabel)
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": testLabel]))
+        addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|[v0]|", options: NSLayoutFormatOptions(), metrics: nil, views: ["v0": testLabel]))
+    }
+    
+}
---
+++ ProductViewer/Offers/OffersViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  ProductViewer
+//
+//  Created by Richard S. Munz on 12/10/17.
+//  Copyright © 2017 Richard S. Munz. All rights reserved.
+//
+
+import UIKit
+
+class OffersViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+
+    let cellId = "cell"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView?.backgroundColor = UIColor.white
+        
+        collectionView?.register(OfferCollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 8
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: UIScreen.main.bounds.width/2.0, height: 100)
+    }
+
+}
