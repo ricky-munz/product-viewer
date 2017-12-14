42b8067f25eb78c6c01fe9094492e92bcaccecdd
Ricky Munz <richard.munz@unos.org>
2017-12-14 13:05:29 -0500

    Add Offer init with JSON struct.

--- ProductViewerTests/ProductViewerTests.swift
+++ ProductViewerTests/ProductViewerTests.swift
@@ -26,11 +26,32 @@ class ProductViewerTests: XCTestCase {
         // Use XCTAssert and related functions to verify your tests produce the correct results.
     }
     
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+    func testOfferInitJson() {
+        let id = "110579"
+        let offerDict: [String: Any?] = [
+            "id": id,
+            "url":"https://product-images.ibotta.com/offer/dUxYcQPeq391-DiywFZF8g-normal.png",
+            "name":"Scotch-Brite® Scrub Dots Non-Scratch Scrub Sponges",
+            "description":"Any variety - 2 ct. pack or larger",
+            "terms":"Rebate valid on Scotch-Brite® Scrub Dots Non-Scratch Scrub Sponges for any variety, 2 ct. pack or larger.",
+            "current_value":"$0.75 Cash Back"
+        ]
+        var offer: Offer
+        
+        do {
+            let jsonData = try JSONSerialization.data(withJSONObject: offerDict, options: .prettyPrinted)
+            print(jsonData)
+            offer = try JSONDecoder().decode(Offer.self, from: jsonData)
+            
+            XCTAssertEqual(id, offer.id)
+            
+        } catch {
+            XCTFail("Failed to decode JSON data using Offer protocol.")
         }
+    }
+    
+    func testOfferToggle() {
+        
     }
     
 }
